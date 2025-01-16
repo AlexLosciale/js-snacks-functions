191bae14bb7128559dc00a3084b610629954b297
AlexLosciale <losciale17@gmail.com>
2025-01-16 15:56:50 +0100

    add:2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,22 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+console.log(names);
 
 // Dichiara la funzione qui.
-
+function letteraIniziale(names) {
+    const iniziale = [];
+    for (let i = 0; i < names.length; i++) {
+        iniziale.push(names[i].charAt(0).toUpperCase());
+    }
+    return iniziale;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const name = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const iniziale = letteraIniziale(names);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+console.log(iniziale); 
